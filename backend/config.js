632b1ccfbaf60b7ad66604255c2e173bb3f49fd1
combfreak45 <113178794+combfreak45@users.noreply.github.com>
2023-12-30 14:49:42 +0530

    first draft

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const MongoDBURL =
+'mongodb+srv://arjit:kedia@books-store-mern.lff6a6o.mongodb.net/books-collection?retryWrites=true&w=majority'
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,34 @@
-const express = require('express')
-const app = express()
+import express from "express";
+import { PORT, MongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModels.js";
+import booksRoute from './routes/booksRoute.js'
+import cors from 'cors'
 
+const app = express();
+
+app.use(express.json())
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET','POST','PUT','DELETE'],
+    allowedHeaders: ['Content-Type']
+}))
+
+app.get("/", (req, res) => {
+  res.status(200).send("HI ITS HOME");
+});
+
+app.use('/books',booksRoute)
+
+mongoose
+  .connect(MongoDBURL)
+  .then(() => {
+    console.log("APP IS CONNECTED");
+    app.listen(PORT, (req, res) => {
+      console.log(`I m listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
