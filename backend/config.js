66013ca6e6a41e11f0896582107bbbc17a5e3c08
Arjit Kedia <113178794+combfreak45@users.noreply.github.com>
2024-01-19 13:17:30 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-export const PORT = 5555;
+export const PORT = https://book-store-backend-5t9yhlumh-arjits-projects-4f88e43c.vercel.app;
 
 export const MongoDBURL =
 'mongodb+srv://arjit:kedia@books-store-mern.lff6a6o.mongodb.net/books-collection?retryWrites=true&w=majority'
