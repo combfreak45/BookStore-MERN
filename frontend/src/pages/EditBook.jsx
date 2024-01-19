53d5057e6d9e5ce23af52c153bcb745b6a98aa77
Arjit Kedia <113178794+combfreak45@users.noreply.github.com>
2024-01-19 14:07:44 +0530

    Update EditBook.jsx

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -17,7 +17,7 @@ const EditBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`https://book-store-backend-5t9yhlumh-arjits-projects-4f88e43c.vercel.app/books/${id}`)
+      .get(`https://book-store-backend-nine.vercel.app/books/${id}`)
       .then((response) => {
         console.log(1)
         setAuthor(response.data.author);
@@ -42,7 +42,7 @@ const EditBook = () => {
     };
     setLoading(true);
     axios
-      .put(`https://book-store-backend-5t9yhlumh-arjits-projects-4f88e43c.vercel.app/books/${id}`, data)
+      .put(`https://book-store-backend-nine.vercel.app/books/${id}`, data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar("Book Edited successfully", { variant: "success" });
